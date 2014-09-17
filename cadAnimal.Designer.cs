0db75f2412788848ffab87263c8c2a3fd3c518c4
singgle <singgle@singlle.mnettec.com.br>
2014-09-16 22:10:02 -0300

    designers

---
+++ cadAnimal.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace singgle.Forms
+{
+    partial class cadAnimal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.nomeAnimalTextBox = new System.Windows.Forms.TextBox();
+            this.brincoAnimalTextBox = new System.Windows.Forms.TextBox();
+            this.motObitoAnimalTextBox = new System.Windows.Forms.TextBox();
+            this.pesoAnimalTextBox = new System.Windows.Forms.TextBox();
+            this.paiAnimalComboBox = new System.Windows.Forms.ComboBox();
+            this.maeAnimalComboBox = new System.Windows.Forms.ComboBox();
+            this.sexoAnimalComboBox = new System.Windows.Forms.ComboBox();
+            this.motBaixaAnimalComboBox = new System.Windows.Forms.ComboBox();
+            this.idRacaAnimalComboBox = new System.Windows.Forms.ComboBox();
+            this.idPropAnimalComboBox = new System.Windows.Forms.ComboBox();
+            this.dtNascAnimalDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.dtBaixaAnimalDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.dt1repAnimalDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.novoButton = new System.Windows.Forms.Button();
+            this.editarButton = new System.Windows.Forms.Button();
+            this.salvarButton = new System.Windows.Forms.Button();
+            this.cancelarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "nomeAnimal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "brincoAnimal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "nascimento";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(136, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "pai";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(130, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "mae";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(128, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "sexo";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(101, 237);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "data baixa";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(91, 264);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "motivo baixa";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(93, 290);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(64, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "motivo obito";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(87, 315);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(70, 13);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "1 reprodução";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(127, 338);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(30, 13);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "peso";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(129, 364);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(28, 13);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "raça";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(94, 391);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(63, 13);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "propriedade";
+            // 
+            // nomeAnimalTextBox
+            // 
+            this.nomeAnimalTextBox.Location = new System.Drawing.Point(163, 71);
+            this.nomeAnimalTextBox.Name = "nomeAnimalTextBox";
+            this.nomeAnimalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nomeAnimalTextBox.TabIndex = 15;
+            // 
+            // brincoAnimalTextBox
+            // 
+            this.brincoAnimalTextBox.Location = new System.Drawing.Point(163, 97);
+            this.brincoAnimalTextBox.Name = "brincoAnimalTextBox";
+            this.brincoAnimalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.brincoAnimalTextBox.TabIndex = 16;
+            // 
+            // motObitoAnimalTextBox
+            // 
+            this.motObitoAnimalTextBox.Location = new System.Drawing.Point(163, 283);
+            this.motObitoAnimalTextBox.Name = "motObitoAnimalTextBox";
+            this.motObitoAnimalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.motObitoAnimalTextBox.TabIndex = 17;
+            // 
+            // pesoAnimalTextBox
+            // 
+            this.pesoAnimalTextBox.Location = new System.Drawing.Point(163, 335);
+            this.pesoAnimalTextBox.Name = "pesoAnimalTextBox";
+            this.pesoAnimalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.pesoAnimalTextBox.TabIndex = 20;
+            // 
+            // paiAnimalComboBox
+            // 
+            this.paiAnimalComboBox.FormattingEnabled = true;
+            this.paiAnimalComboBox.Location = new System.Drawing.Point(163, 149);
+            this.paiAnimalComboBox.Name = "paiAnimalComboBox";
+            this.paiAnimalComboBox.Size = new System.Drawing.Size(121, 21);
+            this.paiAnimalComboBox.TabIndex = 23;
+            this.paiAnimalComboBox.ValueMember = "paiAnimal";
+            // 
+            // maeAnimalComboBox
+            // 
+            this.maeAnimalComboBox.FormattingEnabled = true;
+            this.maeAnimalComboBox.Location = new System.Drawing.Point(163, 176);
+            this.maeAnimalComboBox.Name = "maeAnimalComboBox";
+            this.maeAnimalComboBox.Size = new System.Drawing.Size(121, 21);
+            this.maeAnimalComboBox.TabIndex = 24;
+            this.maeAnimalComboBox.ValueMember = "paiAnimal";
+            // 
+            // sexoAnimalComboBox
+            // 
+            this.sexoAnimalComboBox.FormattingEnabled = true;
+            this.sexoAnimalComboBox.Items.AddRange(new object[] {
+            "F",
+            "M"});
+            this.sexoAnimalComboBox.Location = new System.Drawing.Point(163, 203);
+            this.sexoAnimalComboBox.Name = "sexoAnimalComboBox";
+            this.sexoAnimalComboBox.Size = new System.Drawing.Size(121, 21);
+            this.sexoAnimalComboBox.TabIndex = 25;
+            // 
+            // motBaixaAnimalComboBox
+            // 
+            this.motBaixaAnimalComboBox.FormattingEnabled = true;
+            this.motBaixaAnimalComboBox.Location = new System.Drawing.Point(163, 256);
+            this.motBaixaAnimalComboBox.Name = "motBaixaAnimalComboBox";
+            this.motBaixaAnimalComboBox.Size = new System.Drawing.Size(121, 21);
+            this.motBaixaAnimalComboBox.TabIndex = 26;
+            // 
+            // idRacaAnimalComboBox
+            // 
+            this.idRacaAnimalComboBox.FormattingEnabled = true;
+            this.idRacaAnimalComboBox.Location = new System.Drawing.Point(163, 361);
+            this.idRacaAnimalComboBox.Name = "idRacaAnimalComboBox";
+            this.idRacaAnimalComboBox.Size = new System.Drawing.Size(121, 21);
+            this.idRacaAnimalComboBox.TabIndex = 27;
+            // 
+            // idPropAnimalComboBox
+            // 
+            this.idPropAnimalComboBox.FormattingEnabled = true;
+            this.idPropAnimalComboBox.Location = new System.Drawing.Point(163, 388);
+            this.idPropAnimalComboBox.Name = "idPropAnimalComboBox";
+            this.idPropAnimalComboBox.Size = new System.Drawing.Size(121, 21);
+            this.idPropAnimalComboBox.TabIndex = 28;
+            // 
+            // dtNascAnimalDateTimePicker
+            // 
+            this.dtNascAnimalDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtNascAnimalDateTimePicker.Location = new System.Drawing.Point(163, 123);
+            this.dtNascAnimalDateTimePicker.Name = "dtNascAnimalDateTimePicker";
+            this.dtNascAnimalDateTimePicker.Size = new System.Drawing.Size(121, 20);
+            this.dtNascAnimalDateTimePicker.TabIndex = 29;
+            // 
+            // dtBaixaAnimalDateTimePicker
+            // 
+            this.dtBaixaAnimalDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtBaixaAnimalDateTimePicker.Location = new System.Drawing.Point(163, 230);
+            this.dtBaixaAnimalDateTimePicker.Name = "dtBaixaAnimalDateTimePicker";
+            this.dtBaixaAnimalDateTimePicker.Size = new System.Drawing.Size(121, 20);
+            this.dtBaixaAnimalDateTimePicker.TabIndex = 30;
+            // 
+            // dt1repAnimalDateTimePicker
+            // 
+            this.dt1repAnimalDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dt1repAnimalDateTimePicker.Location = new System.Drawing.Point(163, 309);
+            this.dt1repAnimalDateTimePicker.Name = "dt1repAnimalDateTimePicker";
+            this.dt1repAnimalDateTimePicker.Size = new System.Drawing.Size(121, 20);
+            this.dt1repAnimalDateTimePicker.TabIndex = 31;
+            // 
+            // novoButton
+            // 
+            this.novoButton.Location = new System.Drawing.Point(467, 427);
+            this.novoButton.Name = "novoButton";
+            this.novoButton.Size = new System.Drawing.Size(75, 23);
+            this.novoButton.TabIndex = 32;
+            this.novoButton.Text = "novo";
+            this.novoButton.UseVisualStyleBackColor = true;
+            this.novoButton.Click += new System.EventHandler(this.novoButton_Click);
+            // 
+            // editarButton
+            // 
+            this.editarButton.Location = new System.Drawing.Point(548, 427);
+            this.editarButton.Name = "editarButton";
+            this.editarButton.Size = new System.Drawing.Size(75, 23);
+            this.editarButton.TabIndex = 33;
+            this.editarButton.Text = "editar";
+            this.editarButton.UseVisualStyleBackColor = true;
+            this.editarButton.Click += new System.EventHandler(this.editarButton_Click);
+            // 
+            // salvarButton
+            // 
+            this.salvarButton.Location = new System.Drawing.Point(629, 427);
+            this.salvarButton.Name = "salvarButton";
+            this.salvarButton.Size = new System.Drawing.Size(75, 23);
+            this.salvarButton.TabIndex = 34;
+            this.salvarButton.Text = "salvar";
+            this.salvarButton.UseVisualStyleBackColor = true;
+            this.salvarButton.Click += new System.EventHandler(this.salvarButton_Click);
+            // 
+            // cancelarButton
+            // 
+            this.cancelarButton.Location = new System.Drawing.Point(710, 427);
+            this.cancelarButton.Name = "cancelarButton";
+            this.cancelarButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelarButton.TabIndex = 35;
+            this.cancelarButton.Text = "cancelar";
+            this.cancelarButton.UseVisualStyleBackColor = true;
+            this.cancelarButton.Click += new System.EventHandler(this.cancelarButton_Click);
+            // 
+            // cadAnimal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 470);
+            this.Controls.Add(this.cancelarButton);
+            this.Controls.Add(this.salvarButton);
+            this.Controls.Add(this.editarButton);
+            this.Controls.Add(this.novoButton);
+            this.Controls.Add(this.dt1repAnimalDateTimePicker);
+            this.Controls.Add(this.dtBaixaAnimalDateTimePicker);
+            this.Controls.Add(this.dtNascAnimalDateTimePicker);
+            this.Controls.Add(this.idPropAnimalComboBox);
+            this.Controls.Add(this.idRacaAnimalComboBox);
+            this.Controls.Add(this.motBaixaAnimalComboBox);
+            this.Controls.Add(this.sexoAnimalComboBox);
+            this.Controls.Add(this.maeAnimalComboBox);
+            this.Controls.Add(this.paiAnimalComboBox);
+            this.Controls.Add(this.pesoAnimalTextBox);
+            this.Controls.Add(this.motObitoAnimalTextBox);
+            this.Controls.Add(this.brincoAnimalTextBox);
+            this.Controls.Add(this.nomeAnimalTextBox);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "cadAnimal";
+            this.Text = "cadAnimal";
+            this.Load += new System.EventHandler(this.cadAnimal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox nomeAnimalTextBox;
+        private System.Windows.Forms.TextBox brincoAnimalTextBox;
+        private System.Windows.Forms.TextBox motObitoAnimalTextBox;
+        private System.Windows.Forms.TextBox pesoAnimalTextBox;
+        private System.Windows.Forms.ComboBox paiAnimalComboBox;
+        private System.Windows.Forms.ComboBox maeAnimalComboBox;
+        private System.Windows.Forms.ComboBox sexoAnimalComboBox;
+        private System.Windows.Forms.ComboBox motBaixaAnimalComboBox;
+        private System.Windows.Forms.ComboBox idRacaAnimalComboBox;
+        private System.Windows.Forms.ComboBox idPropAnimalComboBox;
+        private System.Windows.Forms.DateTimePicker dtNascAnimalDateTimePicker;
+        private System.Windows.Forms.DateTimePicker dtBaixaAnimalDateTimePicker;
+        private System.Windows.Forms.DateTimePicker dt1repAnimalDateTimePicker;
+        private System.Windows.Forms.Button novoButton;
+        private System.Windows.Forms.Button editarButton;
+        private System.Windows.Forms.Button salvarButton;
+        private System.Windows.Forms.Button cancelarButton;
+    }
+}
